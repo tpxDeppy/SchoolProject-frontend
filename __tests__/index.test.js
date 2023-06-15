ce0df0d82f18c9c3d02cb55756e698a2956b274c
duncan <duncanuk@gmail.com>
2023-06-15 15:04:17 +0100

    Dont run in watch mode and adds test example

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+test('Adding 2 + 2 equals 4', () => {
+    const result = 2 + 2;
+    expect(result).toBe(4);
+  });
+
+  test('Adding 2 + 2 is not equal to 5', () => {
+    const result = 2 + 2;
+    expect(result).not.toBe(5);
+  });
+  
